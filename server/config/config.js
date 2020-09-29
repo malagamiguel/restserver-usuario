282b6eba5e942dc6f7279a096bb0a0dac146ce17
Miguel.Malaga <m.malaga.ortega@gmail.com>
2020-09-28 21:57:46 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB = `mongodb+srv://user_mike:PxOC0RHt99HdRw9M@cluster0.zuiyo.mongodb.net/cafe`;
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
